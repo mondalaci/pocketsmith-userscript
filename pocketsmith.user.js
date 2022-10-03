5adda35a8101ab9bfb8cd30725edbd14505b3ba0
László Monda <laci@monda.hu>
2022-10-03 19:38:38 +0200

    Fix top bar positions.

--- pocketsmith.user.js
+++ pocketsmith.user.js
@@ -16,11 +16,11 @@ setInterval(() => { // Keepalive session
 
 
 $('<style>').text(`
-/* main menu */
+/* main menu (dashboard, transactions, calendar, ...) */
 .main-menu {height:34px}
 body > .sidebar, body > .sidebar ~ .breadcrumb {top: 34px}
 
-/* breadcrumb */
+/* breadcrumb (graph icons, magnifier icon) */
 body > .breadcrumb {height:30px}
 .breadcrumb h1 {padding:0.5rem 1rem}
 .breadcrumb .graph-buttons > a {line-height: 2.8rem}
@@ -29,17 +29,22 @@ body > .breadcrumb {height:30px}
 /* graph container */
 body > .graph-container {top:-34px; height:350px}
 
-/* toolbar */
-body > .toolbar {height:32px; padding:0.2rem}
-body.position-fixed > .toolbar {top:64px}
-fixed.flight.toolbar {top:-75px}
+/* toolbar (add, tools, export, ...) */
+body                       > .fixed.flight.toolbar {height:32px; padding:0.2rem}
+body:not([position-fixed]) > .fixed.flight.toolbar {top:-75px}
+body.position-fixed        > .fixed.flight.toolbar {top:64px}
 
-body.position-fixed > .page-header {top:96px}
+/* page header (date, merchant, amount, ...) */
+body:not([position-fixed]) > .page-header {top:-75px}
+body.position-fixed        > .page-header {top:96px}
+
+/* transaction list */
+body:not([position-fixed]) > .transactions_content .list-view-outer {top:-81px}
+body.position-fixed        > .transactions_content .list-view-outer {top:-140px}
 
 .list-view-outer {padding: 0}
 .list-view-outer:first {padding: 8px 0px 0px 0px}
 .transactions-table-footer {height:3.6rem; line-height:3rem}
-.transactions_content .list-view-outer {top:-163px}
 body.compact .transactions-table .row .cell {padding:0.5em 0}
 .transactions-table-header .row .cell {line-height: 2.5rem}
 .transactions-table .row .cell.amount .currency-code-info {display: inline-block}
